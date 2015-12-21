4fa3ee28bb10df027e0879f691f26f9cc9782c0a
William Jackson <william@subtlecoolness.com>
2015-12-21 11:00:25 -0600

    Add some metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='rw_rate_all',
     version='1.0.0',
+    author='William Jackson',
+    author_email='william@subtlecoolness.com',
+    url='https://github.com/williamjacksn/rw_rate_all',
+    description='Rate songs on Rainwave',
+    license='MIT License',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
